7d6a54a58b7e1eb6112b29dc31f8edbc68219607
alexandra <alexandra.ragunovich@gmail.com>
2026-06-17 13:15:14 +0300

    fix: added account updates for user

--- src/main/java/com/travel/user_service/service/EmailService.java
+++ src/main/java/com/travel/user_service/service/EmailService.java
@@ -12,7 +12,6 @@ public class EmailService {
     private final JavaMailSender mailSender;
 
     public void sendPasswordResetEmail(String to, String token) {
-        // Ссылка, которая будет вести на ваш фронтенд
         String resetUrl = "http://localhost:5173/reset-password?token=" + token;
 
         SimpleMailMessage message = new SimpleMailMessage();
